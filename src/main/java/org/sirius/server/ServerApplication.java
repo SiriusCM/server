a5145c2e3f4a16f63203ff36e64460de4e2acda6
SiriusCM <963937371@qq.com>
2024-03-16 18:01:40 +0800

    改为single服务器

--- src/main/java/org/sirius/server/ServerApplication.java
+++ src/main/java/org/sirius/server/ServerApplication.java
@@ -7,11 +7,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
 @EnableAsync
 @EnableCaching
 @EnableScheduling
 @EnableTransactionManagement
+@SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
