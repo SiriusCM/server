0fb7f5c050f7a9eaf8ff9e834e75d0c189a9e481
高连棣 <963937371@qq.com>
2025-03-13 21:59:12 +0800

    策划配置bean修改

--- src/main/java/com/sirius/server/conf/ExcelConfig.java
+++ src/main/java/com/sirius/server/conf/ExcelConfig.java
@@ -4,16 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Configuration
 public class ExcelConfig {
+
+    public static Map<Integer, ExcelTest> mapExcelTest = new HashMap<>();
+
+    public static void reload(JdbcTemplate jdbcTemplate) {
+        mapExcelTest = jdbcTemplate.queryForList("select * from excel_test", ExcelTest.class).stream()
+                .collect(Collectors.toMap(ExcelTest::sn, c -> c));
+    }
     
     @Bean
     public Map<Integer, ExcelTest> mapExcelTest(JdbcTemplate jdbcTemplate) {
         return jdbcTemplate.queryForList("select * from excel_test", ExcelTest.class).stream()
                 .collect(Collectors.toMap(ExcelTest::sn, c -> c));
     }
-    
 }
--- src/main/resources/templates/excelConfig.ftl
+++ src/main/resources/templates/excelConfig.ftl
@@ -9,6 +9,16 @@ import java.util.stream.Collectors;
 @Configuration
 public class ExcelConfig {
     {% for name in nameList %}
+    public static Map<Integer, {{name['className']}}> map{{name['className']}} = new HashMap<>();
+    {% endfor %}
+
+    public static void reload(JdbcTemplate jdbcTemplate) {
+        {% for name in nameList %}
+        map{{name['className']} = jdbcTemplate.queryForList("select * from {{name['dbName']}}", {{name['className']}.class).stream()
+        .collect(Collectors.toMap({{name['className']}::sn, c -> c));
+        {% endfor %}
+    }
+    {% for name in nameList %}
     @Bean
     public Map<Integer, {{name['className']}}> map{{name['className']}}(JdbcTemplate jdbcTemplate) {
         return jdbcTemplate.queryForList("select * from {{name['dbName']}}", {{name['className']}}.class).stream()
